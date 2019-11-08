d3ba9c3bf1c13599743ebe4eb63eea4127d7defc
gleb <gleby-hleby@inbox.ru>
2019-11-09 00:33:35 +0300

    Scripted auto-commit on change (2019-11-09 00:33:35) by gitwatch.sh

--- src/main/java/FlightData.java
+++ src/main/java/FlightData.java
@@ -1,3 +1,9 @@
 public class FlightData {
-    
+    public int delays;
+    public int cancelations;
+
+    public FlightData(int delays, int cancelations) {
+        this.delays = delays;
+        this.cancelations = cancelations;
+    }
 }
