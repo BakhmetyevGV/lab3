bde71ec66d2e02d13dcb96ac946f579cc0e17092
gleb <gleby-hleby@inbox.ru>
2019-11-09 00:46:24 +0300

    Scripted auto-commit on change (2019-11-09 00:46:24) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -3,15 +3,13 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-import java.io.Serializable;
-
 public class AirportTime{
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
         JavaRDD<String> a =  sparkContext.textFile("Flights.csv");
-        JavaPairRDD<JavaRDD<String>, AirportSerializable> b = a.mapToPair(e -> {
+        JavaPairRDD<JavaRDD<String>, FlightSerializable> b = a.mapToPair(e -> {
             return 0;
         });
     }
--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -1,22 +1,31 @@
-import java.io.IOException;
 import java.io.Serializable;
 
-public class AirportSerializable  implements Serializable {
+public class FlightSerializable implements Serializable {
     public int flightsCount;
     public int delaysCount;
     public int maxDelay;
     public int cancelationCount;
 
-    public AirportSerializable(FlightData oData) {
-        this.delaysCount      =  oData.delays == 0 ? 0 : 1;
-        this.cancelationCount =  oData.cancelations;
-        this.maxDelay         =  oData.delays;
+    public FlightSerializable(int flightsCount, int delaysCount, int maxDelay, int cancelationCount) {
+        this.flightsCount = flightsCount;
+        this.delaysCount = delaysCount;
+        this.maxDelay = maxDelay;
+        this.cancelationCount = cancelationCount;
+    }
+
+    public FlightSerializable(FlightData data) {
+        this.delaysCount      =  data.delays == 0 ? 0 : 1;
+        this.cancelationCount =  data.cancelations;
+        this.maxDelay         =  data.delays;
         this.flightsCount     =  1;
     }
 
-    public static AirportSerializable reduce(AirportSerializable a, AirportSerializable b){
-        return new AirportSerializable(
-                
-        )
+    public static FlightSerializable reduce(FlightSerializable a, FlightSerializable b){
+        return new FlightSerializable(
+                a.flightsCount + b.flightsCount,
+                a.delaysCount + b.delaysCount,
+                Math.max(a.maxDelay, b.maxDelay),
+                a.cancelationCount + b.cancelationCount
+        );
     }
 }
