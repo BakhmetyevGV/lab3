01f6aa259f8b644578b2ee45fa320f6ab7ad65bb
gleb <gleby-hleby@inbox.ru>
2019-11-09 00:12:44 +0300

    Scripted auto-commit on change (2019-11-09 00:12:44) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -3,13 +3,15 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.io.Serializable;
+
 public class AirportTime{
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
         JavaRDD<String> a =  sparkContext.textFile("Flights.csv");
-        JavaPairRDD<JavaRDD<String>, > b = a.mapToPair(e -> {
+        JavaPairRDD<JavaRDD<String>, AirportSerializable> b = a.mapToPair(e -> {
             return 0;
         });
     }
