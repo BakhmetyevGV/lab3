dd1324b37f31b51149e6504c98d4a84f0cfb24e7
gleb <gleby-hleby@inbox.ru>
2019-11-09 00:27:17 +0300

    Scripted auto-commit on change (2019-11-09 00:27:17) by gitwatch.sh

--- src/main/java/AirportSerializable.java
+++ src/main/java/AirportSerializable.java
@@ -5,12 +5,12 @@ public class AirportSerializable  implements Serializable {
     public int flightsCount;
     public int delaysCount;
     public int maxDelay;
-    public int cancelationsCount;
+    public int cancelationCount;
 
-    public AirportSerializable(int flightsCount, int delaysCount, int maxDelay, int cancelationsCount) {
+    public AirportSerializable(int flightsCount, int delaysCount, int maxDelay, int cancelationCount) {
         this.flightsCount = flightsCount;
         this.delaysCount = delaysCount;
         this.maxDelay = maxDelay;
-        this.cancelationsCount = cancelationsCount;
+        this.cancelationCount = cancelationCount;
     }
 }
