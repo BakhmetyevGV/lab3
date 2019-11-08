22bc509ebb295023fddc4140e27463d7336b72e1
gleb <gleby-hleby@inbox.ru>
2019-11-09 00:20:56 +0300

    Scripted auto-commit on change (2019-11-09 00:20:56) by gitwatch.sh

--- src/main/java/AirportSerializable.java
+++ src/main/java/AirportSerializable.java
@@ -3,5 +3,6 @@ import java.io.Serializable;
 
 public class AirportSerializable  implements Serializable {
     public int flightsCount;
-    
+    public int delaysCount;
+    public int flightsCount;
 }
