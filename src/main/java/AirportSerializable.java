3506013deba11fd5677fd1bc12d1bd4a562fa216
gleb <gleby-hleby@inbox.ru>
2019-11-09 00:19:23 +0300

    Scripted auto-commit on change (2019-11-09 00:19:23) by gitwatch.sh

--- src/main/java/AirportSerializable.java
+++ src/main/java/AirportSerializable.java
@@ -2,7 +2,5 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public class AirportSerializable  implements Serializable {
-    public final void writeObject(Object obj) throws IOException{
-        
-    }
+    
 }
