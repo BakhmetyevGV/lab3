bee8cc2bd29a2ab412b0297e208226d188be8425
gleb <gleby-hleby@inbox.ru>
2019-11-09 00:04:24 +0300

    Scripted auto-commit on change (2019-11-09 00:04:24) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -1,5 +1,6 @@
 public class AirportTime{
     public static void main(String[] args){
-        
+        SparkConf conf = new SparkConf().setAppName("lab5");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
